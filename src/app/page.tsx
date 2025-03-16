e1e9d5e3d39b27df8a91aa55fb470dfce79bf31f
timonjagi <timothynjihia@gmail.com>
2025-03-16 18:25:00 +0300

    Comment out Projects import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "@/components/Sidebar";
 import { AboutMe } from "@/components/AboutMe";
-import { Projects } from "@/components/Projects";
+// import { Projects } from "@/components/Projects";
 import { Experience } from "@/components/Experience";
 
 export default function Home() {
